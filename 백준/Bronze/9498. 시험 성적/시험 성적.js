b4df7950ba7ca84d76081589d9193f8b9f5b7f36
SeoHee3478 <78894678+SeoHee3478@users.noreply.github.com>
2023-09-06 23:34:21 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let score = parseInt(input[0]);
+
+if(score >= 90) console.log('A');
+else if(score >=80) console.log('B');
+else if(score >=70)console.log('C');
+else if(score >=60) console.log('D');
+else console.log('F');
+
+
