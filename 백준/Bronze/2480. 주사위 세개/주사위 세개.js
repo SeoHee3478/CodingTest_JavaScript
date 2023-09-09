e943a3b9d8032aa2d2429404b621643efec4387f
SeoHee3478 <78894678+SeoHee3478@users.noreply.github.com>
2023-09-09 21:20:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,14 +1,14 @@
+// fs 모듈을 이용해 파일 전체를 읽어와 문자열로 저장하기
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let [a,b,c] = input[0].split(' ').map(Number);
 
+// 세 개의 수가 모두 같은 경우
 if(a==b && b==c) console.log(10000+a*1000);
+// 세 개의 수가 전부 같지는 않지만, 두 개의 수가 같은 경우
 else if(a==b) console.log(1000+a*100);
 else if(b==c) console.log(1000+b*100);
 else if(c==a) console.log(1000+c*100);
-else {
-    if(a>b && a>c) console.log(a*100);
-    else if(b>a && b>c) console.log(b*100);
-    else console.log(c*100);
-}
+// 세 개의 수가 전부 다른 경우
+else console.log(Math.max(a,b,c)*100);
