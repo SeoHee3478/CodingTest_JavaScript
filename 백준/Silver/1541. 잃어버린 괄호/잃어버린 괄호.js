9c8be5a4eeeac9c2f5d46427b70c760381bfcf2e
SeoHee3478 <78894678+SeoHee3478@users.noreply.github.com>
2023-10-20 16:11:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// 뺄셈(-) 연산자를 기준으로 나누어 여러 그룹 만들기
+let groups = input[0].split('-');
+let answer = 0;
+for (let i = 0; i<groups.length; i++){
+    // 각 그룹 내부에서 덧셈(+)연산 적용
+    let cur = groups[i].split('+').map(Number).reduce((a,b)=>a+b);
+    if(i==0) answer += cur; //첫번째 그룹은 항상 덧셈(+)
+    else answer -=cur; // 두 번째 그룹부터 뺄셈(-)
+}
+
+console.log(answer);
